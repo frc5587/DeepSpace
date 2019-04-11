e082a3a6a7f3a0927da530481d46c7f4087b4787
Brendan Doney <bre.doney@gmail.com>
2019-04-11 10:52:48 -0400

    Use higher refresh-rate worker for limelight

--- src/main/java/org/frc5587/deepspace/commands/routines/Limelight.java
+++ src/main/java/org/frc5587/deepspace/commands/routines/Limelight.java
@@ -7,39 +7,58 @@ import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.command.Command;
 
 public class Limelight extends Command {
-
     // current horizontal angle
-    private NetworkTableEntry tx = NetworkTableInstance.getDefault().getTable("limelight").getEntry("tx");
+    private static final NetworkTableEntry tx = NetworkTableInstance.getDefault().getTable("limelight").getEntry("tx");
+
+    private LimelightWorker limelightWorker;
 
     public Limelight() {
-
+        this.limelightWorker = new LimelightWorker();
     }
 
     @Override
     protected void initialize() {
+        limelightWorker.start();
         Robot.DRIVETRAIN.enableTurnPID(true);
     }
 
     @Override
     protected void execute() {
-        var currentHeading = Robot.DRIVETRAIN.getHeading(180.0);
-        double desiredAngle = currentHeading - tx.getDouble(0);
-        Robot.DRIVETRAIN.setTurnPID(desiredAngle);
     }
 
     @Override
     protected void end() {
+        limelightWorker.interrupt();
         Robot.DRIVETRAIN.enableTurnPID(false);
         Robot.DRIVETRAIN.vbusLR(0, 0);
     }
 
     @Override
-    protected void interrupted() {
-        end();
+    protected boolean isFinished() {
+        return !limelightWorker.isInterrupted();
     }
 
-    @Override
-    protected boolean isFinished() {
-        return false;
+    private static class LimelightWorker extends Thread {
+        private double lastAngleError;
+
+        public LimelightWorker() {
+            this.lastAngleError = Double.NaN;
+        }
+
+        @Override
+        public void run() {
+            while (!interrupted()) {
+                // Check if the new error is equal so as not to recompute PID
+                var newError = tx.getDouble(0);
+                if (newError != lastAngleError) {
+                    // Send information to the drivetrain now
+                    var currentHeading = Robot.DRIVETRAIN.getHeading(180.0);
+                    double desiredAngle = currentHeading - newError;
+                    Robot.DRIVETRAIN.setTurnPID(desiredAngle);
+
+                    lastAngleError = newError;
+                }
+            }
+        }
     }
 }
